7180807fab6938cae24a6abad33d4f02560e45f1
J-HowHuang <howardhuang2001@gmail.com>
2020-09-16 10:03:30 +0800

    Add delete button to each game info page

--- public/js/history.js
+++ public/js/history.js
@@ -546,7 +546,18 @@ function renderMatchInfoById(gid){
     $(".plot-info #name").contents().filter(function () {
         return this.nodeType == 3;
     })[0].nodeValue = game.name
-    $(".plot-info #pos").html(formatDate(game.date));
+    var deleteButton = 
+    '<button class="btn" id="delete" data-toggle="modal" data-target="#confirmDelete">\
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor"\
+            xmlns="http://www.w3.org/2000/svg">\
+            <path\
+                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />\
+            <path fill-rule="evenodd"\
+                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />\
+        </svg>\
+    </button>';
+
+    $(".plot-info #pos").html((formatDate(game.date))+deleteButton);
 
     var row = ""
     Object.keys(matchDetailedStatsTitle).forEach(function (field) {
@@ -576,6 +587,7 @@ function renderMatchInfoById(gid){
             })
         })
     });
+    $("#deleteGame").on('click', () => deleteGame(gid));
 }
 
 function renderPlayerInfoById(pid) {
@@ -618,6 +630,11 @@ function renderPlayerInfoById(pid) {
 
 function initMatchInfo() {
     $(".plot-info .info-overview ul").empty();
+    $(".plot-info #name").contents().filter(function () {
+        return this.nodeType == 3;
+    })[0].nodeValue = '';
+    $(".plot-info #pos").empty()
+    $("#deleteGame").off('click');
     Object.keys(matchDetailedStatsTitle).forEach(function (key) {
         var row = ""
         row += "<li class=\"list-group-item\" id=\"" + key + '-title' + "\"><span id=\"" + key + "\"> </span>" + matchDetailedStatsTitle[key].title + "</li>";
@@ -688,6 +705,22 @@ function initPlayerInfo() {
             $('.plot-info #'+key+'-table').attr('style', '');
         })
     });
+}
+
+function deleteGame(gid){
+    $.ajax({
+        url: '/api/game/'+gid,
+        headers: {
+        },
+        method: 'DELETE',
+        timeout: 0
+    })
+    .done(function(res){
+        location.reload();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
 
 function getServeErrorRate(records) {
